7c73a9849e41a7426d6e425d7b70979869c07c6e
Pierre Van der Eecken <pierre.vandereecken@gmail.com>
2012-04-09 14:16:32 +0200

    navigation + lots of commented code

--- _js/jquery.parallax-1.1.js
+++ _js/jquery.parallax-1.1.js
@@ -11,13 +11,14 @@ http://www.gnu.org/licenses/gpl.html
 */
 
 //function that places the navigation in the center of the window
-function RepositionNav(){
-	var windowHeight = $(window).height(); //get the height of the window
-	var navHeight = $('#nav').height() / 2;
-	var windowCenter = (windowHeight / 2); 
-	var newtop = windowCenter - navHeight;
-	//$('#nav').css({"top": newtop}); //set the new top position of the navigation list
-}
+
+//function RepositionNav(){
+//	var windowHeight = $(window).height(); //get the height of the window
+//	var navHeight = $('#nav').height() / 2;
+//	var windowCenter = (windowHeight / 2); 
+//	var newtop = windowCenter - navHeight;
+//	$('#nav').css({"top": newtop}); //set the new top position of the navigation list
+//}
 
 
 (function( $ ){
