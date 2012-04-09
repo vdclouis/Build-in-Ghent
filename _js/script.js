a5f19a483fe1efef4b2381458434c3ec3a28270b
Louis Van de Calseyde <louis@yaska.eu>
2012-04-09 14:16:24 +0200

    toby & t-ak - electric dejavu

--- _js/script.js
+++ _js/script.js
@@ -1,4 +1,53 @@
-/* LOAD PRODUCTS */
+/* GLOBAL VARIABLES */
+var selectedLanguage = "nl";
+function setLanguageFromLink(languageLink){
+	/* GET LANGUAGE FROM REL */
+	var language = $(languageLink).attr('rel');
+	
+	/* SET LANGUAGE FROM REL --> LINK */
+	setLanguage(language);
+}
+/* SET LANGUAGE FOR THE WEBAPP
+--> Update HTML
+*/
+function setLanguage(language){
+	/* CALL METHOD  setLanguageToStorage
+	first argument: the language
+	*/
+	setLanguageToStorage(language);
+	
+	/* LOAD SPECIFIED CONTENT IN LANGUAGE */
+	$("#language-indicator").html('<h1>' + language + '</h1>');
+	
+	
+	/* SET THE NEW LANGUAGE TO THE GLOBAL VARIABLE */
+	selectedLanguage = language;
+	
+}
+function getLanguageFromStorage(){
+	var language = "nl";
+	/* GET LANGUAGE FROM LOCALSTORAGE */
+	if (Modernizr.localstorage) {		
+		if(localStorage.getItem('stonesfromghent-language') == null){
+			setLanguageToStorage('nl');
+		}
+		else{
+			language = localStorage.getItem('stonesfromghent-language');
+		}
+	}
+	return language;	
+}
+function setLanguageToStorage(language){
+	if (Modernizr.localstorage) {
+		localStorage.setItem('stonesfromghent-language',language);
+	}
+	else {
+		setLanguage('nl');
+	}	
+}
+
+
+/* load texts */
 function loadTexts(url){
 	$.ajax({
 		url:url,
@@ -10,19 +59,22 @@ function loadTexts(url){
 		error:loadTextsError
 	});
 }
-function loadTextError(xhr, message, optional){	
+
+function loadTextsError(xhr, message, optional){	
 	$(".text").html("Error loading products XML file!");
 }
+
 function loadTextsSuccess(data, xhr){
-	parseProducts(data);
+	parseTexts(data);
 }
+
 function parseTexts(xml){
 	var title;
 	var text;
 	
     var htmlContent = "";
 
-    $(xml).find('en').each(function () {
+    $(xml).find(localStorage.getItem('stonesfromghent-language')).each(function () {
 
         title = $(this).find('title').text();
         text = $(this).find('text').text();
@@ -36,13 +88,14 @@ function parseTexts(xml){
     $(".text").html(htmlContent);
 }
 
-
+/* document ready */
 $(function () {
-	//NO CACHE FOR AJAX
 	$.ajaxSetup ({
 		cache: false
 	});
 	
-	/* LOAD PRODUCTS */
-	loadProducts("_resources/_xml/texts.xml");
+	loadTexts("_resources/_xml/texts.xml");
+	
+	/* SET THE LANGUAGE AFTER PAGE LOAD OR REFRESH */
+	setLanguage(getLanguageFromStorage());
 })
