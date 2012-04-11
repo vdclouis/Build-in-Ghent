84df1e409923de0b8b67f9237778ddb01c7b9a9e
Pierre Van der Eecken <pierre.vandereecken@gmail.com>
2012-04-11 21:41:58 +0200

    contactfooterbar

--- _js/effects.js
+++ _js/effects.js
@@ -1,9 +1,14 @@
 $(function () {
+	//jquery test
+	console.log("It's Alive!");
+
+	//gives the first nav element current status (red color)
 	$('#nav li:first-child').addClass('current');
 
+	//hides the second page
 	$('#parkeergebouw').hide();
 	
-	console.log('test');
+	//navigation with the bulletnavs
 	$('.bulletnav .rightbullet').click( function () {
 		$('#gerechtsgebouw').hide();
 		$('#parkeergebouw').fadeIn(1000);
@@ -22,20 +27,30 @@ $(function () {
 	});
 	
 	$('#contactbig').hide();
-//	$('#contactsmall').click(function() {
-//		$('#contactbig').toggle("fold", { direction: "vertical" }, 1000);
-//		return false;
-//	});
-//	$('#collapse').click(function() {
-//		$('#contactbig').toggle("fold", { direction: "vertical" }, 1000);
-//		return false;
-//	});
 
-	$('#contactsmall').click(function() {
-		$(this).animate({
-			'width':'400px',
-			'height':'100px'
+	$('#contactsmall p').click(function() {
+		$('#contact').animate({
+			'width':'100%',
+			'height':'100px',
+			'border-top-left-radius': '50px',
+			'border-top-right-radius': '50px',
+			'right':'0',
 		}, 500);
+		$('#contactbig').fadeIn(500);
+		console.log('contact clicked!');
+		return false;
+	});
+	
+	$('#collapse').click(function() {
+		$('#contactbig').fadeOut(500);
+		$('#contact').animate({
+			'width':'50px',
+			'height':'10px',
+			'border-top-left-radius': '10px',
+			'border-top-right-radius': '10px',
+			'right': '50px'
+		}, 500);
+		console.log('contact hidden!');
 		return false;
 	});
 });
