2d417c4e2f09ddf3016f950046235ff0466624ec
Pierre Van der Eecken <pierre.vandereecken@gmail.com>
2012-04-11 17:32:47 +0200

    gebouwen updated

--- _js/effects.js
+++ _js/effects.js
@@ -3,16 +3,16 @@ $(function () {
 	
 	console.log('test');
 	$('#rightbullet').click( function () {
-		$('#gerechtsgebouw').fadeOut(100);
-		$('#parkeergebouw').fadeIn(800);
+		$('#gerechtsgebouw').hide();
+		$('#parkeergebouw').fadeIn(1000);
 		$(this).animate({ backgroundColor: '#2096d3'}, 500);
 		$('#leftbullet').animate({ backgroundColor: '#b2b2b2'}, 500);
 		console.log('rightbullet clicked!');
 		return false;
 	});
 	$('#leftbullet').click( function () {
-		$('#parkeergebouw').fadeOut(1);
-		$('#gerechtsgebouw').fadeIn(800);
+		$('#parkeergebouw').hide();
+		$('#gerechtsgebouw').fadeIn(1000);
 		$(this).animate({ backgroundColor: '#2096d3'}, 500);
 		$('#rightbullet').animate({ backgroundColor: '#b2b2b2'}, 500);
 		console.log('leftbullet clicked!');
