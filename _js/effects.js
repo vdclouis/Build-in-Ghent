f1d36f68f2198b79e41cab950b219a6af36e027e
Pierre Van der Eecken <pierre.vandereecken@gmail.com>
2012-04-11 11:21:52 +0200

    jquery ui

--- _js/effects.js
+++ _js/effects.js
@@ -4,14 +4,16 @@ $(function () {
 	$('#rightbullet').click( function () {
 		$('#gerechtsgebouw').fadeOut(100);
 		$('#parkeergebouw').fadeIn(800);
-		$(this).animate({'background-color': '#2096d3'}, 500);
-		$('#leftbullet').animate({'background-color': '#b2b2b2'}, 500);
+		$(this).animate({ backgroundColor: '#2096d3'}, 500);
+		$('#leftbullet').animate({ backgroundColor: '#b2b2b2'}, 500);
 		console.log('rightbullet clicked!');
 		return false;
 	});
 	$('#leftbullet').click( function () {
 		$('#parkeergebouw').fadeOut(1);
 		$('#gerechtsgebouw').fadeIn(800);
+		$(this).animate({ backgroundColor: '#2096d3'}, 500);
+		$('#rightbullet').animate({ backgroundColor: '#b2b2b2'}, 500);
 		console.log('leftbullet clicked!');
 		return false;
 	});
