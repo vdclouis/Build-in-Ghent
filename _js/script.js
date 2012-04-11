6279c1bd7221990adc6862689736db1dea50c1a1
Louis Van de Calseyde <louis@yaska.eu>
2012-04-11 14:05:44 +0200

    dynamic navigation

--- _js/script.js
+++ _js/script.js
@@ -23,8 +23,16 @@ function parseTexts(xml){
 	var title;
 	var text;
 	
+	var home;
+	var gebouw;
+	var fotos;
+	var praktisch;
+	var video;
+	var nieuws;
+	
     var htmlMain = "";
     var htmlSub = "";
+    var htmlNav = "";
 		
 	var textlang = localStorage.getItem('stonesfromghent-language');
 	
@@ -40,7 +48,7 @@ function parseTexts(xml){
     }); // end each loop  
     
     //sub gebouw
-    $(xml).find(textlang).each(function () {
+    $(xml).find('texts').find(textlang).each(function () {
     	$(this).find('sub').each(function () {
             title = $(this).find('title').text();
             text = $(this).find('text').text();
@@ -49,10 +57,30 @@ function parseTexts(xml){
     htmlSub += '<h1>' + title + '</h1>' + '<p>' + text + '</p>';
        
     }); // end each loop
+    
+    nav
+    $(xml).find(textlang).each(function () {
+    	$(this).find('nav').each(function () {
+    		home = $(this).find('home').text();
+    		gebouw = $(this).find('gebouw').text();
+    		fotos = $(this).find('fotos').text();
+    		praktisch = $(this).find('praktisch').text();
+    		video = $(this).find('video').text();
+    		nieuws = $(this).find('nieuws').text();
+    	});
+    htmlNav += '<li><a href="#home" title="Next Section">' + home +'</a></li>'
+    			+'<li><a href="#gebouw" title="Next Section">' + gebouw +'</a></li>'
+    			+'<li><a href="#fotos" title="Next Section">' + fotos +'</a></li>'
+    			+'<li><a href="#praktisch" title="Next Section">' + praktisch +'</a></li>'
+    			+'<li><a href="#video" title="Next Section">' + video +'</a></li>'
+    			+'<li><a href="#nieuws" title="Next Section">' + nieuws +'</a></li>'
+    
+    }); //eo each loop
 
     //Write to container
     $(".main").html(htmlMain);
     $(".sub").html(htmlSub);
+    $("nav").html(htmlNav);
 }
 
 /* document ready */
