2fe39ce17f82c8dce27eaf6389c154cf48dfa5d4
Louis Van de Calseyde <louis@yaska.eu>
2012-04-11 14:59:06 +0200

    commented dynamic nav

--- _js/script.js
+++ _js/script.js
@@ -23,16 +23,16 @@ function parseTexts(xml){
 	var title;
 	var text;
 	
-	var home;
-	var gebouw;
-	var fotos;
-	var praktisch;
-	var video;
-	var nieuws;
+//	var home;
+//	var gebouw;
+//	var fotos;
+//	var praktisch;
+//	var video;
+//	var nieuws;
 	
     var htmlMain = "";
     var htmlSub = "";
-    var htmlNav = "";
+//    var htmlNav = "";
 		
 	var textlang = localStorage.getItem('stonesfromghent-language');
 	
@@ -58,29 +58,30 @@ function parseTexts(xml){
        
     }); // end each loop
     
-    nav
-    $(xml).find(textlang).each(function () {
-    	$(this).find('nav').each(function () {
-    		home = $(this).find('home').text();
-    		gebouw = $(this).find('gebouw').text();
-    		fotos = $(this).find('fotos').text();
-    		praktisch = $(this).find('praktisch').text();
-    		video = $(this).find('video').text();
-    		nieuws = $(this).find('nieuws').text();
-    	});
-    htmlNav += '<li><a href="#home" title="Next Section">' + home +'</a></li>'
-    			+'<li><a href="#gebouw" title="Next Section">' + gebouw +'</a></li>'
-    			+'<li><a href="#fotos" title="Next Section">' + fotos +'</a></li>'
-    			+'<li><a href="#praktisch" title="Next Section">' + praktisch +'</a></li>'
-    			+'<li><a href="#video" title="Next Section">' + video +'</a></li>'
-    			+'<li><a href="#nieuws" title="Next Section">' + nieuws +'</a></li>'
-    
-    }); //eo each loop
+    //nav
+//    $(xml).find(textlang).each(function () {
+//    	$(this).find('nav').each(function () {
+//    		home = $(this).find('home').text();
+//    		gebouw = $(this).find('gebouw').text();
+//    		fotos = $(this).find('fotos').text();
+//    		praktisch = $(this).find('praktisch').text();
+//    		video = $(this).find('video').text();
+//    		nieuws = $(this).find('nieuws').text();
+//    	});
+//    	
+//    htmlNav += '<li><a href="#home" title="Next Section">' + home +'</a></li>'
+//    			+'<li><a href="#gebouw" title="Next Section">' + gebouw +'</a></li>'
+//    			+'<li><a href="#fotos" title="Next Section">' + fotos +'</a></li>'
+//    			+'<li><a href="#praktisch" title="Next Section">' + praktisch +'</a></li>'
+//    			+'<li><a href="#video" title="Next Section">' + video +'</a></li>'
+//    			+'<li><a href="#nieuws" title="Next Section">' + nieuws +'</a></li>';
+//    
+//    }); //eo each loop
 
     //Write to container
     $(".main").html(htmlMain);
     $(".sub").html(htmlSub);
-    $("nav").html(htmlNav);
+//    $("ul#nav").html(htmlNav);
 }
 
 /* document ready */
