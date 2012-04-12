81ab64e4e3590e994512ea91d22895d555c8b6e6
Louis Van de Calseyde <louis@yaska.eu>
2012-04-12 10:05:01 +0200

    stylin shizzlin

--- _js/script.js
+++ _js/script.js
@@ -68,7 +68,7 @@ function parseTexts(xml){
     		title = $(this).find('title').text();
     		date = $(this).find('date').text();
     		
-    		htmlNews += '<tr><td>' + date + '</td><td>' + title + '</td></tr>';
+    		htmlNews += '<tr><td><p>' + date + '</p></td><td><p>' + title + '</p></td></tr>';
     	});
     });
     
@@ -119,16 +119,16 @@ $(function () {
 	
 	loadTexts("_resources/_xml/texts.xml");
 	
-	$("table#newz").click(function () {
-		console.log("BANG");
-		$(xml).find(textlang).each(function () {
-			$(this).find('item').each(function () {
-				content = $(this).find('content').text();
-				
-				htmlNewsContent += '<p>' + content + '</p>';
-			});
-		});
-		
-		$("#newsContent").html(htmlNewsContent).show();
-	});
+//	$("table#newz").click(function () {
+//		console.log("BANG");
+//		$(xml).find(textlang).each(function () {
+//			$(this).find('item').each(function () {
+//				content = $(this).find('content').text();
+//				
+//				htmlNewsContent += '<p>' + content + '</p>';
+//			});
+//		});
+//		
+//		$("#newsContent").html(htmlNewsContent).show();
+//	});
 })
